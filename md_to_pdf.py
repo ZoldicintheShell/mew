19b5b4342dd5e34435c2daf8cc622d4f02ba41fa
zztaouil <ztaouil@student.1337.ma>
2023-09-29 08:20:12 +0100

    Changes to take in css styling and output path as arguments

--- md_to_pdf.py
+++ md_to_pdf.py
@@ -1,45 +1,60 @@
 import markdown2
+import re
 from weasyprint import HTML, CSS
 from PIL import Image
-import re
+from sys import argv, stderr
 
-# Charger le contenu du fichier Markdown
-with open("input.md", "r", encoding="utf-8") as md_file:
-    md_content = md_file.read()
+# Path arguments are relative paths to the root of mewtwo py projects 
+#
+# python3 md_to_pdf.py <style_path> <output_path>
 
-# Convertir le Markdown en HTML
-html_content = markdown2.markdown(md_content)
 
-# Rechercher les balises d'images Markdown et remplacer par des balises HTML
-html_content_with_images = re.sub(r'!\[.*?\]\((.*?)\)', r'<img src="\1" />', html_content)
+def main():
+    if (len(argv) != 3):
+        print('Arguments error.', file=stderr)
+        return 1
 
-# Charger l'image de fond
-background_image = Image.open("Background/background_git.png")
+    style_path = argv[1]
+    output_path = argv[2]
 
-# Charger le contenu du fichier CSS
-with open("style.css", "r", encoding="utf-8") as css_file:
-    custom_css = css_file.read()
+    # Charger le contenu du fichier Markdown
+    with open("input.md", "r", encoding="utf-8") as md_file:
+        md_content = md_file.read()
 
-# Générer le PDF à partir du HTML avec mise en page personnalisée
-final_html = f"""
-<!DOCTYPE html>
-<html>
-<head>
-<style>
-{custom_css}
-</style>
-</head>
-<body>
-{html_content_with_images}
-</body>
-</html>
-"""
+    # Convertir le Markdown en HTML
+    html_content = markdown2.markdown(md_content)
 
-html = HTML(string=final_html, base_url="./")  # L'argument base_url aide à résoudre les chemins relatifs
-pdf_bytes = html.write_pdf()
+    # Rechercher les balises d'images Markdown et remplacer par des balises HTML
+    html_content_with_images = re.sub(r'!\[.*?\]\((.*?)\)', r'<img src="\1" />', html_content)
 
-# Enregistrer le PDF résultant
-with open("output.pdf", "wb") as pdf_file:
-    pdf_file.write(pdf_bytes)
+    # Charger le contenu du fichier CSS
+    with open(style_path, "r", encoding="utf-8") as css_file:
+        custom_css = css_file.read()
 
-print("Conversion terminée.")
+    # Générer le PDF à partir du HTML avec mise en page personnalisée
+    final_html = f"""
+    <!DOCTYPE html>
+    <html>
+    <head>
+    <style>
+    {custom_css}
+    </style>
+    </head>
+    <body>
+    {html_content_with_images}
+    </body>
+    </html>
+    """
+
+    html = HTML(string=final_html, base_url="./")  # L'argument base_url aide à résoudre les chemins relatifs
+    pdf_bytes = html.write_pdf()
+
+    # Enregistrer le PDF résultant
+    with open(output_path, "wb") as pdf_file:
+        pdf_file.write(pdf_bytes)
+
+    print("Conversion terminée.")
+
+
+if __name__ == '__main__':
+    main()
